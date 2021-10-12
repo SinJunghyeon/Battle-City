f6b90349031e90388ba41409998ef36f80f4595b
SinJunghyeon <tlslrs1994@gmail.com>
2021-10-12 17:50:53 +0900

    start

---
+++ _WinMain/MainGame.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Config.h"
+#include "GameEntity.h"
+
+// 우리가 구성할 게임 월드
+class Image;
+class MainGame : public GameEntity
+{
+private:
+	// 타이머
+	HANDLE hTimer;
+
+	// UI
+	char text[128];
+
+	int mousePosX;
+	int mousePosY;
+	int clickedMousePosX;
+	int clickedMousePosY;
+
+	// 백버퍼
+	Image* backBuffer;
+
+public:
+	HRESULT Init();
+	void Update();
+	void Render(HDC hdc);
+	void Release();
+
+	LRESULT MainProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+};
---
+++ _WinMain/Tank.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Config.h"
+#include "GameObject.h"
+#include "Ammo.h"
+
+class Tank : public GameObject
+{
+private:
+
+
+public:
+
+
+	Tank();
+	~Tank();
+};
+
